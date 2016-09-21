3236f828e6645c36a3fb76939b48759df9b7e837
Pierre Filliolaud <pierre.filliolaud@gmail.com>
2016-09-21 12:08:52 +0200

    add entity product

---
+++ src/main/java/com/mycompany/myapp/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.mycompany.myapp.repository;
+
+import com.mycompany.myapp.domain.Product;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Product entity.
+ */
+@SuppressWarnings("unused")
+public interface ProductRepository extends MongoRepository<Product,String> {
+
+}
---
+++ src/main/java/com/mycompany/myapp/web/rest/ProductResource.java
@@ -0,0 +1,137 @@
+package com.mycompany.myapp.web.rest;
+
+import com.codahale.metrics.annotation.Timed;
+import com.mycompany.myapp.domain.Product;
+import com.mycompany.myapp.service.ProductService;
+import com.mycompany.myapp.web.rest.util.HeaderUtil;
+import com.mycompany.myapp.web.rest.util.PaginationUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * REST controller for managing Product.
+ */
+@RestController
+@RequestMapping("/api")
+public class ProductResource {
+
+    private final Logger log = LoggerFactory.getLogger(ProductResource.class);
+        
+    @Inject
+    private ProductService productService;
+
+    /**
+     * POST  /products : Create a new product.
+     *
+     * @param product the product to create
+     * @return the ResponseEntity with status 201 (Created) and with body the new product, or with status 400 (Bad Request) if the product has already an ID
+     * @throws URISyntaxException if the Location URI syntax is incorrect
+     */
+    @RequestMapping(value = "/products",
+        method = RequestMethod.POST,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) throws URISyntaxException {
+        log.debug("REST request to save Product : {}", product);
+        if (product.getId() != null) {
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("product", "idexists", "A new product cannot already have an ID")).body(null);
+        }
+        Product result = productService.save(product);
+        return ResponseEntity.created(new URI("/api/products/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert("product", result.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * PUT  /products : Updates an existing product.
+     *
+     * @param product the product to update
+     * @return the ResponseEntity with status 200 (OK) and with body the updated product,
+     * or with status 400 (Bad Request) if the product is not valid,
+     * or with status 500 (Internal Server Error) if the product couldnt be updated
+     * @throws URISyntaxException if the Location URI syntax is incorrect
+     */
+    @RequestMapping(value = "/products",
+        method = RequestMethod.PUT,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product) throws URISyntaxException {
+        log.debug("REST request to update Product : {}", product);
+        if (product.getId() == null) {
+            return createProduct(product);
+        }
+        Product result = productService.save(product);
+        return ResponseEntity.ok()
+            .headers(HeaderUtil.createEntityUpdateAlert("product", product.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * GET  /products : get all the products.
+     *
+     * @param pageable the pagination information
+     * @return the ResponseEntity with status 200 (OK) and the list of products in body
+     * @throws URISyntaxException if there is an error to generate the pagination HTTP headers
+     */
+    @RequestMapping(value = "/products",
+        method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    public ResponseEntity<List<Product>> getAllProducts(Pageable pageable)
+        throws URISyntaxException {
+        log.debug("REST request to get a page of Products");
+        Page<Product> page = productService.findAll(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/products");
+        return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
+    }
+
+    /**
+     * GET  /products/:id : get the "id" product.
+     *
+     * @param id the id of the product to retrieve
+     * @return the ResponseEntity with status 200 (OK) and with body the product, or with status 404 (Not Found)
+     */
+    @RequestMapping(value = "/products/{id}",
+        method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    public ResponseEntity<Product> getProduct(@PathVariable String id) {
+        log.debug("REST request to get Product : {}", id);
+        Product product = productService.findOne(id);
+        return Optional.ofNullable(product)
+            .map(result -> new ResponseEntity<>(
+                result,
+                HttpStatus.OK))
+            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    /**
+     * DELETE  /products/:id : delete the "id" product.
+     *
+     * @param id the id of the product to delete
+     * @return the ResponseEntity with status 200 (OK)
+     */
+    @RequestMapping(value = "/products/{id}",
+        method = RequestMethod.DELETE,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    public ResponseEntity<Void> deleteProduct(@PathVariable String id) {
+        log.debug("REST request to delete Product : {}", id);
+        productService.delete(id);
+        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert("product", id.toString())).build();
+    }
+
+}
